371ce9506a88593f7f0878b648ae144569b5756a
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-12-14 12:46:37 +0530

    creating the proposal model

--- src/pages/proposer/protected/proposerHome/tabPages/otherProposalViewModel/OtherProposalViewModel.tsx
+++ src/pages/proposer/protected/proposerHome/tabPages/otherProposalViewModel/OtherProposalViewModel.tsx
@@ -1,10 +1,11 @@
-import { Modal, Image, Skeleton, Alert } from "antd";
+import { Modal, Image, Skeleton } from "antd";
 import {
   ProposerOtherProposalType,
   ProposerOtherProposerType,
 } from "../../../../../../utility/typesAndEnum";
 import { useProposerGetOtherProposal } from "../../../../../../services/proposer";
 import { useEffect, useState } from "react";
+import { getCountryLabel } from "../../../../../../utility/Methods";
 
 type OtherProposalViewModelProp = {
   isModalOpen: boolean;
@@ -36,12 +37,6 @@ export default function OtherProposalViewModel(
   const getContent = () => {
     if (otherProposalQuery.isPending) {
       return <Skeleton active />;
-    } else if (otherProposalQuery.isError) {
-      return (
-        <div>
-          <Alert message="Proposal data loading Error" type="error" />
-        </div>
-      );
     } else if (otherProposalQuery.isSuccess) {
       return (
         <div>
@@ -60,7 +55,124 @@ export default function OtherProposalViewModel(
                 " " +
                 prop.otherProposer?.lastName}
             </span>
-            <div className="w-full h-[2px] bg-slate-200 rounded-lg my-2"></div>
+            <div className="bg-cyan-100 rounded-lg p-4 mt-4">
+              <span className="text-lg font-semibold">
+                Personal Information
+              </span>
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <span className="font-semibold">Basic</span>
+                <div className="bg-sl flex flex-row gap-4 mt-2">
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Ethnicity :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.ethnicity}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Religion :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.religion}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Age :{" "}
+                      </div>
+                      <div className="w-full">{otherProposalData?.age}</div>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Caste :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.caste ?? "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Civil Status :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.civilStatus}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Height :{" "}
+                      </div>
+                      <div className="w-full">{otherProposalData?.height}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <span className="font-semibold">Residency</span>
+                <div className="bg-sl flex flex-row gap-4 mt-2">
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Country :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.country
+                          ? getCountryLabel(otherProposalData.country)
+                          : "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        State / District :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.stateOrDistrict}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        City :{" "}
+                      </div>
+                      <div className="w-full">{otherProposalData?.city}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <span className="font-semibold">Education and Profession</span>
+                <div className="bg-sl flex flex-row gap-4 mt-2">
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Education :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.education}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Profession :{" "}
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.profession}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -74,7 +186,9 @@ export default function OtherProposalViewModel(
       okButtonProps={{ className: "hidden" }}
       width={1000}
     >
-      <div className="my-8 p-4">{getContent()}</div>
+      <div className="my-8 p-4 max-h-[65vh] overflow-y-auto">
+        {getContent()}
+      </div>
     </Modal>
   );
 }
