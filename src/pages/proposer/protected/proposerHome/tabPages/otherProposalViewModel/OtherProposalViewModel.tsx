63e3023a2639ba001a1061382d57da34d457494a
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-12-14 14:36:32 +0530

    creating the other proposal view

--- src/pages/proposer/protected/proposerHome/tabPages/otherProposalViewModel/OtherProposalViewModel.tsx
+++ src/pages/proposer/protected/proposerHome/tabPages/otherProposalViewModel/OtherProposalViewModel.tsx
@@ -1,5 +1,6 @@
 import { Modal, Image, Skeleton } from "antd";
 import {
+  ProposerFoodPreferenceEnum,
   ProposerOtherProposalType,
   ProposerOtherProposerType,
 } from "../../../../../../utility/typesAndEnum";
@@ -50,22 +51,23 @@ export default function OtherProposalViewModel(
             </div>
           </div>
           <div className="bg-slate-50 rounded-lg p-4">
-            <span className="capitalize text-2xl font-semibold">
+            <span className="capitalize text-2xl font-bold">
               {prop.otherProposer?.firstName +
                 " " +
                 prop.otherProposer?.lastName}
             </span>
+
             <div className="bg-cyan-100 rounded-lg p-4 mt-4">
               <span className="text-lg font-semibold">
                 Personal Information
               </span>
               <div className="my-4 p-4 bg-white rounded-lg">
                 <span className="font-semibold">Basic</span>
-                <div className="bg-sl flex flex-row gap-4 mt-2">
+                <div className="flex flex-row gap-4 mt-2">
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
                       <div className="font-medium w-full text-right">
-                        Ethnicity :{" "}
+                        Ethnicity
                       </div>
                       <div className="w-full">
                         {otherProposalData?.ethnicity}
@@ -73,31 +75,29 @@ export default function OtherProposalViewModel(
                     </div>
                     <div className="flex flex-row gap-4 py-2">
                       <div className="font-medium w-full text-right">
-                        Religion :{" "}
+                        Religion
                       </div>
                       <div className="w-full">
                         {otherProposalData?.religion}
                       </div>
                     </div>
                     <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
-                      <div className="font-medium w-full text-right">
-                        Age :{" "}
+                      <div className="font-medium w-full text-right">Age </div>
+                      <div className="w-full">
+                        {otherProposalData?.age} years old
                       </div>
-                      <div className="w-full">{otherProposalData?.age}</div>
                     </div>
                   </div>
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
-                      <div className="font-medium w-full text-right">
-                        Caste :{" "}
-                      </div>
+                      <div className="font-medium w-full text-right">Caste</div>
                       <div className="w-full">
                         {otherProposalData?.caste ?? "-"}
                       </div>
                     </div>
                     <div className="flex flex-row gap-4 py-2">
                       <div className="font-medium w-full text-right">
-                        Civil Status :{" "}
+                        Civil Status
                       </div>
                       <div className="w-full">
                         {otherProposalData?.civilStatus}
@@ -105,7 +105,7 @@ export default function OtherProposalViewModel(
                     </div>
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
                       <div className="font-medium w-full text-right">
-                        Height :{" "}
+                        Height
                       </div>
                       <div className="w-full">{otherProposalData?.height}</div>
                     </div>
@@ -115,11 +115,11 @@ export default function OtherProposalViewModel(
 
               <div className="my-4 p-4 bg-white rounded-lg">
                 <span className="font-semibold">Residency</span>
-                <div className="bg-sl flex flex-row gap-4 mt-2">
+                <div className="flex flex-row gap-4 mt-2">
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
                       <div className="font-medium w-full text-right">
-                        Country :{" "}
+                        Country
                       </div>
                       <div className="w-full">
                         {otherProposalData?.country
@@ -129,7 +129,7 @@ export default function OtherProposalViewModel(
                     </div>
                     <div className="flex flex-row gap-4 py-2">
                       <div className="font-medium w-full text-right">
-                        State / District :{" "}
+                        State / District
                       </div>
                       <div className="w-full">
                         {otherProposalData?.stateOrDistrict}
@@ -138,9 +138,7 @@ export default function OtherProposalViewModel(
                   </div>
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
-                      <div className="font-medium w-full text-right">
-                        City :{" "}
-                      </div>
+                      <div className="font-medium w-full text-right">City</div>
                       <div className="w-full">{otherProposalData?.city}</div>
                     </div>
                   </div>
@@ -149,11 +147,11 @@ export default function OtherProposalViewModel(
 
               <div className="my-4 p-4 bg-white rounded-lg">
                 <span className="font-semibold">Education and Profession</span>
-                <div className="bg-sl flex flex-row gap-4 mt-2">
+                <div className="flex flex-row gap-4 mt-2">
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
                       <div className="font-medium w-full text-right">
-                        Education :{" "}
+                        Education
                       </div>
                       <div className="w-full">
                         {otherProposalData?.education}
@@ -163,11 +161,183 @@ export default function OtherProposalViewModel(
                   <div className="w-full">
                     <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
                       <div className="font-medium w-full text-right">
-                        Profession :{" "}
+                        Profession
                       </div>
                       <div className="w-full">
                         {otherProposalData?.profession}
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <span className="font-semibold">Habits</span>
+                <div className="flex flex-row gap-4 mt-2">
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Drinking
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.drinking ? "Yes" : "No"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Smoking
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.smoking ? "Yes" : "No"}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Food Preference
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.foodPreference ===
+                        ProposerFoodPreferenceEnum.NonVegetarian
+                          ? "Non vegetarian"
+                          : otherProposalData?.foodPreference}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-cyan-100 rounded-lg p-4 mt-4">
+              <span className="text-lg font-semibold">Family Information</span>
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <div className=" flex flex-row gap-4">
+                  <div className="w-full">
+                    <span className="font-semibold">Father</span>
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg mt-2">
+                      <div className="font-medium w-full text-right">
+                        Ethnicity
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherEthnicity}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Religion
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherReligion}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">Caste</div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherCaste ?? "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Profession
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherProfession ?? "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Country of Residence
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherCountryOfResidence
+                          ? getCountryLabel(
+                              otherProposalData.fatherCountryOfResidence,
+                            )
+                          : "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Additional Information
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.fatherAdditionalInfo ?? "-"}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <span className="font-semibold">Mother</span>
+                    <div className="flex flex-row gap-4 py-2 bg-slate-100 rounded-lg mt-2">
+                      <div className="font-medium w-full text-right">
+                        Ethnicity
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.motherEthnicity}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Religion
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.motherReligion}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">Caste</div>
+                      <div className="w-full">
+                        {otherProposalData?.motherCaste ?? "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Profession
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.motherProfession ?? "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4  py-2 bg-slate-100 rounded-lg">
+                      <div className="font-medium w-full text-right">
+                        Country of Residence
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.motherCountryOfResidence
+                          ? getCountryLabel(
+                              otherProposalData.motherCountryOfResidence,
+                            )
+                          : "-"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-4 py-2">
+                      <div className="font-medium w-full text-right">
+                        Additional Information
+                      </div>
+                      <div className="w-full">
+                        {otherProposalData?.motherAdditionalInfo ?? "-"}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-cyan-100 rounded-lg p-4 mt-4">
+              <span className="text-lg font-semibold">
+                Horoscope Information
+              </span>
+
+              <div className="my-4 p-4 bg-white rounded-lg">
+                <div className="w-1/2">
+                  <div className="flex flex-row gap-4 py-2 mr-2 bg-slate-100 rounded-lg">
+                    <div className="font-medium w-full text-right">
+                      Horoscope Matching
+                    </div>
+                    <div className="w-full">
+                      {otherProposalData?.horoscopeMatching
+                        ? "Required"
+                        : "Not required"}
                     </div>
                   </div>
                 </div>
