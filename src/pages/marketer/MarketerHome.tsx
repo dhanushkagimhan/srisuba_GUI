ef95ba0ac4754513705c8609c806be25ddc32aac
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-01-04 21:09:24 +0530

    added intro sections

--- src/pages/marketer/MarketerHome.tsx
+++ src/pages/marketer/MarketerHome.tsx
@@ -10,32 +10,49 @@ export default function MarketerHome() {
   useEffect(() => {
     mainLayoutState.setData({
       navMenu: MainLayoutNavEnum.marketerRegister,
-      showFooter: false,
+      showFooter: true,
       showMarketing: false,
       logoLink: "/",
     });
   }, []);
 
   return (
-    <div className="flex md:flex-row flex-col md:justify-between">
-      <div className="w-full md:pt-20 lg:pl-20 md:pr-10">
-        <h1 className="md:text-6xl text-3xl text-neutral-900">
-          Earn with Srisuba
-        </h1>
-        <p className="text-neutral-900 md:-mt-5 md:text-xl font-medium">
-          You can become a affiliate marketer with Srisuba.
-        </p>
-        <p className="text-neutral-900 md:text-xl font-medium">
-          You can earn{" "}
-          <span className="bg-yellow-200">
-            Rs. {proposalPriceState.price ? proposalPriceState.price / 10 : ""}{" "}
-            (LKR)
-          </span>{" "}
-          for the every proposal suggestion.
-        </p>
+    <div>
+      <div className="flex md:flex-row flex-col md:justify-between">
+        <div className="w-full md:pt-20 lg:pl-20 md:pr-10">
+          <h1 className="md:text-6xl text-3xl text-neutral-900">
+            Earn with Srisuba
+          </h1>
+          <p className="text-neutral-900 md:-mt-5 md:text-xl font-medium">
+            You can become a affiliate marketer with Srisuba.
+          </p>
+          <p className="text-neutral-900 md:text-xl font-medium">
+            You can earn{" "}
+            <span className="bg-yellow-200">
+              Rs.{" "}
+              {proposalPriceState.price ? proposalPriceState.price / 10 : ""}{" "}
+              (LKR)
+            </span>{" "}
+            for the every proposal suggestion.
+          </p>
+        </div>
+        <div className="w-full md:pt-20 pt-10 xl:pl-40">
+          <MarketerLogin />
+        </div>
       </div>
-      <div className="w-full md:pt-20 pt-10 xl:pl-40">
-        <MarketerLogin />
+
+      <div className="flex flex-row justify-center mt-10">
+        <div>
+          <h3>Learn more about srisuba.com Affiliate marketing program</h3>
+          <iframe
+            className="sm:w-[560px] sm:h-[315px] w-[255px] h-[144px]"
+            src="https://www.youtube-nocookie.com/embed/0rLOzvDAz6Y?si=D0Xmm5h6IgoJp-ow"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
